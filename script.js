c8724a4cdf626c6a9ab8861cce8018359befc14d
HarrisM00 <Harrismahmood014@gmail.com>
2024-10-21 13:19:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1')
+    ourRequest.onload = function(){
+    var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+};
+ourRequest.send()  
+});
